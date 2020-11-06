b4fe7e48993285b076eb3fa740021944c41bc645
kaiser <13858889915@163.com>
2020-11-07 00:43:58 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const { ModuleFilenameHelpers } = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+};
