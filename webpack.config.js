d7decbec5a75fa557581442d6dce7adca4b00d27
kaiser <13858889915@163.com>
2020-11-07 02:21:16 +0800

    reset

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-const { ModuleFilenameHelpers } = require("webpack");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "凯森",
+      template: "src/assets/index.html",
+    }),
+    [new MiniCssExtractPlugin()],
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
 };
